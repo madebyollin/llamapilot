2d7d9b0d766402d685dbc68231b35920425b42a3
Ollin Boer Bohan <madebyollin@gmail.com>
2023-03-12 16:24:14 -0700

    reloads the model every keystroke

--- llamapilot/extension.js
+++ llamapilot/extension.js
@@ -2,42 +2,28 @@ const vscode = require('vscode');
 const child = require('child_process');
 
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('llamapilot.engage', function () {
-		const editor = vscode.window.activeTextEditor;
-		const config = vscode.workspace.getConfiguration('llamapilot');
+	const config = vscode.workspace.getConfiguration('llamapilot');
 
-		if (editor) {
-			const doc = editor.document;
-			const sel = editor.selection;
+	const llamaComplete = {
+		provideInlineCompletionItems: async (doc, pos, ctx, tok) => {
+			const prompt = doc.getText(new vscode.Range(
+				new vscode.Position(0, 0), pos));
+			vscode.window.showInformationMessage(`🦙("${prompt}")...`);
 
-			const prompt = doc.getText(sel);
-			// vscode.window.showInformationMessage(`🦙("${prompt}")...`);
-			vscode.window.withProgress(
-				{
-				  location: vscode.ProgressLocation.Notification,
-				  title: `Running LLaMA on "${prompt}"...`,
-				  cancellable: false,
-				},
-				async (progress, token) => {
-					progress.report({increment: 0});
-					let res = child.execFileSync('./main', [
-						'-m', config.llamaModel,
-						'--prompt', prompt,
-						'-n', config.tokenCount
-					], {cwd: config.llamaFolder}).toString();
-					const start = res.indexOf("\n" + prompt) + prompt.length + 1;
-					const end = res.indexOf("[end of text]", start);
-					let resSliced = res.slice(start, end);
-					editor.edit(editBuilder => {
-						editBuilder.insert(sel.end, resSliced);
-						progress.report({increment: 100});
-					});
-				}
-			);
-		}
-	});
+			let res = child.execFileSync('./main', [
+				'-m', config.llamaModel,
+				'--prompt', prompt,
+				'-n', config.tokenCount
+			], {cwd: config.llamaFolder}).toString();
+			const start = res.indexOf("\n" + prompt) + prompt.length + 1;
+			const end = res.indexOf("[end of text]", start);
+			let output = res.slice(start, end);
+			let items = [{text: output, insertText: output, range: new vscode.Range(pos.translate(0, output.length), pos)}];
+			return {items};
+		},
+	};
 
-	context.subscriptions.push(disposable);
+	vscode.languages.registerInlineCompletionItemProvider({pattern: "**"}, llamaComplete);
 }
 
 function deactivate() {}
